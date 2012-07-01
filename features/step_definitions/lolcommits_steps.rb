471a05e4c13bd4362a8c9ed0bc6bc0b838732671
Matthew Rothenberg <mrothenberg@gmail.com>
2012-06-30 23:36:37 -0400

    first rudimentary cuke tests

--- features/step_definitions/lolcommits_steps.rb
+++ features/step_definitions/lolcommits_steps.rb
@@ -1,1 +1,20 @@
-# Put your step definitions here
+include FileUtils
+
+
+Given /^a git repository named "(.*?)"$/ do |repo_name|
+  repo_dir = File.join current_dir, repo_name
+  mkdir_p repo_dir
+  Dir.chdir repo_dir do
+    sh "git init ."
+  end
+end
+
+Given /^the git repository named "(.*?)" has no "(.*?)" hook$/ do |repo_name, hook_name|
+  hook_file = File.join current_dir, repo_name, ".git", "hooks", hook_name
+  delete(hook_file) if File.exists? hook_file
+end
+
+Given /^the git repository named "(.*?)" has a "(.*?)" hook$/ do |repo_name, hook_name|
+  hook_file = File.join current_dir, repo_name, ".git", "hooks", hook_name
+  touch(hook_file) if not File.exists? hook_file
+end
