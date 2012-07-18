2f876872d3c7abda98f853dbb42cd6ecdf0bb121
Ken Mazaika <kmazaika@gmail.com>
2012-07-17 20:23:41 -0400

    Clean up git_info code a little.

--- lib/lolcommits/git_info.rb
+++ lib/lolcommits/git_info.rb
@@ -2,11 +2,12 @@ module Lolcommits
   class GitInfo
     attr_accessor :sha, :message, :repo
     def initialize
-      g = Git.open('.')
-      commit = g.log.first
+      git    = Git.open('.')
+      commit = git.log.first
+
       self.message = commit.message.split("\n").first
-      self.sha = commit.sha[0..10]
-      self.repo = Git.open('.').remote.url.split(':').last.gsub(/\.git\Z/, '')
+      self.sha     = commit.sha[0..10]
+      self.repo    = git.remote.url.split(':').last.gsub(/\.git\Z/, '')
     end
   end
 end
