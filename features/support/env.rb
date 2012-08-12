70df60af50b6fc051a9abf612b96dbc6dced1274
Matthew Rothenberg <mrothenberg@gmail.com>
2012-08-12 18:29:15 -0400

    fake $HOME instead of setting $LOLCOMMITS_DIR
    this should make for more reliable tests as its closer to a normal user
    environment

--- features/support/env.rb
+++ features/support/env.rb
@@ -10,15 +10,19 @@ LIB_DIR = File.join(File.expand_path(File.dirname(__FILE__)),'..','..','lib')
 Before do
   # Using "announce" causes massive warnings on 1.9.2
   @puts = true
-  @original_rubylib = ENV['RUBYLIB']
   @aruba_timeout_seconds = 20
+
+  @original_rubylib = ENV['RUBYLIB']
   ENV['RUBYLIB'] = LIB_DIR + File::PATH_SEPARATOR + ENV['RUBYLIB'].to_s
 
   @original_fakecapture = ENV['LOLCOMMITS_FAKECAPTURE']
   ENV['LOLCOMMITS_FAKECAPTURE'] = "1"
 
-  @original_loldir = ENV['LOLCOMMITS_DIR']
-  ENV['LOLCOMMITS_DIR'] = File.expand_path( File.join(current_dir, ".lolcommits") )
+  # @original_loldir = ENV['LOLCOMMITS_DIR']
+  # ENV['LOLCOMMITS_DIR'] = File.expand_path( File.join(current_dir, ".lolcommits") )
+
+  @original_home = ENV['HOME']
+  ENV['HOME'] = File.expand_path( current_dir )
 
   ENV['LAUNCHY_DRY_RUN'] = 'true'
 end
@@ -26,20 +30,7 @@ end
 After do
   ENV['RUBYLIB'] = @original_rubylib
   ENV['LOLCOMMITS_FAKECAPTURE'] = @original_fakecapture
-  ENV['LOLCOMMITS_DIR'] = @original_loldir
+  # ENV['LOLCOMMITS_DIR'] = @original_loldir
+  ENV['HOME'] = @original_home
   ENV['LAUNCHY_DRY_RUN'] = nil
 end
-
-# ###moved the below into env for everything
-# Before('@simulate-env') do
-#   @original_fakecapture = ENV['LOLCOMMITS_FAKECAPTURE']
-#   ENV['LOLCOMMITS_FAKECAPTURE'] = "1"
-
-#   @original_loldir = ENV['LOLCOMMITS_DIR']
-#   ENV['LOLCOMMITS_DIR'] = File.join(current_dir, ".lolcommits")
-# end
-
-# After('@simulate-env') do
-#   ENV['LOLCOMMITS_FAKECAPTURE'] = @original_fakecapture
-#   ENV['LOLCOMMITS_DIR'] = @original_loldir
-# end
