5522f89c205a08cf5461823ad92c9b041d8e9dc4
Matthew Rothenberg <mrothenberg@gmail.com>
2013-01-31 13:00:39 -0500

    add some debug logging for git parsing
    might help debug #88

--- lib/lolcommits/git_info.rb
+++ lib/lolcommits/git_info.rb
@@ -1,13 +1,23 @@
 module Lolcommits
   class GitInfo
+    include Methadone::CLILogging
     attr_accessor :sha, :message, :repo_internal_path
+
     def initialize
+      debug "GitInfo: attempting to read local repository"
       g    = Git.open('.')
+      debug "GitInfo: reading commits logs"
       commit = g.log.first
+      debug "GitInfo: most recent commit is '#{commit}'"
 
       self.message = commit.message.split("\n").first
       self.sha     = commit.sha[0..10]
       self.repo_internal_path = g.repo.path
+      
+      debug "GitInfo: parsed the following values from commit:"
+      debug "GitInfo: \t#{self.message}"
+      debug "GitInfo: \t#{self.sha}"
+      debug "GitInfo: \t#{self.repo_internal_path}" 
     end
   end
 end
