c07170652869916d2388a43a050d44f6a6210e12
Mike Splain <msplain@paypal.com>
2013-02-22 13:42:42 -0500

    Lets just use quotes instead!

--- lib/lolcommits/capture_mac.rb
+++ lib/lolcommits/capture_mac.rb
@@ -1,7 +1,7 @@
 module Lolcommits
   class CaptureMac < Capturer
     def capture_device_string
-      @capture_device.nil? ? nil : "-d #{@capture_device.gsub(' ','\ ')}"
+      @capture_device.nil? ? nil : "-d \"#{@capture_device}\""
     end
 
     def capture
