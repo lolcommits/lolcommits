e1f98a9c609e57eb7189c5677a57d4929bf8a9d1
Marvin Marcelo <mrclmrvn@gmail.com>
2013-09-29 16:57:42 +0800

    Added configuration setter from the plugin
     * Bump version

--- lib/lolcommits/plugins/lol_yammer.rb
+++ lib/lolcommits/plugins/lol_yammer.rb
@@ -1,20 +1,11 @@
 require 'yammer'
-require 'addressable/uri'
+require 'rest_client'
 
-# Getting a Token
-#
-# * Replace the Consumer Key placeholder in this URL: https://www.yammer.com/dialog/oauth?client_id=CONSUMER_KEY, with the Consumer Key from step 1. Then open the URL in a browser.
-# * After signing into Yammer and authorizing the application, you will be redirected to https://www.yammer.com?code=CODE. Record the value of the code parameter.
-# * Replace the values in this URL with the Consumer Key and Secret (step 1), and the code (step 3): https://www.yammer.com/oauth2/access_token.json?client_id=CONSUMER_KEY&client_secret=CONSUMER_SECRET&code=CODE. Then open the URL in a browser.
-# * Your browser should now be displaying a JSON response. Capture the value of the token member as this will be used to make API calls.
+# https://developer.yammer.com/oauth2-quickstart/
 
 YAMMER_CLIENT_ID = 'bgORyeKtnjZJSMwp8oln9g'
 YAMMER_CLIENT_SECRET = 'oer2WdGzh74a5QBbW3INUxblHK3yg9KvCZmiBa2r0'
 YAMMER_ACCESS_TOKEN_URL = "https://www.yammer.com/oauth2/access_token.json"
-
-# https://www.yammer.com/oauth2/access_token.json?client_id=bgORyeKtnjZJSMwp8oln9g&client_secret=oer2WdGzh74a5QBbW3INUxblHK3yg9KvCZmiBa2r0&code=0LtdfJRtPA05PFXXVLZg
-
-# sample access_token 7eMte1lGK2P6iKKEhaToA
 
 module Lolcommits
 
@@ -25,10 +16,21 @@ module Lolcommits
       self.name    = 'yammer'
       self.default = false
       self.options.concat(['access_token'])
+      puts "Open the URL below and copy the `code` param from query after redirected, enter it as `access_token`:"
+      puts "https://www.yammer.com/dialog/oauth?client_id=#{YAMMER_CLIENT_ID}"
+    end
+
+    def set_access_token(code)
+      url = "#{YAMMER_ACCESS_TOKEN_URL}?client_id=%s&client_secret=%s&code=%s" % [YAMMER_CLIENT_ID, YAMMER_CLIENT_SECRET, code]
+      plugdebug "access_token url: #{url}"
+      result = JSON.parse(RestClient.get(url))
+      result["access_token"]["token"]
     end
 
     def run
       if configuration['access_token'].nil?
+        puts "Run configuration first:"
+        puts "lolcommits --config --plugin yammer"
         puts "Missing Yammer Credentials - Skipping post"
         return
       end
@@ -53,7 +55,9 @@ module Lolcommits
       rescue => e
         retries -= 1
         retry if retries > 0
-        puts "\t ! --> Status not posted - #{e.message}"
+        puts "Status not posted - #{e.message}"
+        puts "Try running config again:"
+        puts "\tlolcommits --config --plugin yammer"
       end
     end
   end
