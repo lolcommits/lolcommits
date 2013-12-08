e685cb91f6d37cca36d1c42d3cb437bef9ac1977
Marvin Marcelo <mrclmrvn@gmail.com>
2013-12-08 09:50:34 +0800

    use configure_options!

--- lib/lolcommits/plugins/lol_yammer.rb
+++ lib/lolcommits/plugins/lol_yammer.rb
@@ -2,9 +2,8 @@ require 'yammer'
 require 'rest_client'
 
 # https://developer.yammer.com/oauth2-quickstart/
-
-YAMMER_CLIENT_ID = 'bgORyeKtnjZJSMwp8oln9g'
-YAMMER_CLIENT_SECRET = 'oer2WdGzh74a5QBbW3INUxblHK3yg9KvCZmiBa2r0'
+YAMMER_CLIENT_ID        = 'bgORyeKtnjZJSMwp8oln9g'
+YAMMER_CLIENT_SECRET    = 'oer2WdGzh74a5QBbW3INUxblHK3yg9KvCZmiBa2r0'
 YAMMER_ACCESS_TOKEN_URL = "https://www.yammer.com/oauth2/access_token.json"
 
 module Lolcommits
@@ -19,11 +18,30 @@ module Lolcommits
       !configuration['access_token'].nil?
     end
 
-    def set_access_token(code)
+    def get_access_token(code)
       url = "#{YAMMER_ACCESS_TOKEN_URL}?client_id=%s&client_secret=%s&code=%s" % [YAMMER_CLIENT_ID, YAMMER_CLIENT_SECRET, code]
       plugdebug "access_token url: #{url}"
       result = JSON.parse(RestClient.get(url))
-      result["access_token"]["token"]
+      {'access_token' => result["access_token"]["token"]}
+    end
+
+    def configure_options!
+      options = super
+      if options['enabled'] == true
+        if auth_config = get_access_token
+          options.merge!(auth_config)
+        else
+          return
+        end
+      end
+      options
+    end
+
+    def yammer_client
+      @yammer_client ||= Yammer.configure do |c|
+        c.client_id = YAMMER_CLIENT_ID
+        c.client_secret = YAMMER_CLIENT_SECRET
+      end
     end
 
     def run
@@ -33,17 +51,10 @@ module Lolcommits
       post = "#{commit_msg} #lolcommits"
       puts "Yammer post: #{post}" unless self.runner.capture_stealth
 
-      Yammer.configure do |c|
-        c.client_id = YAMMER_CLIENT_ID
-        c.client_secret = YAMMER_CLIENT_SECRET
-      end
-
-      client = Yammer::Client.new(:access_token  => configuration['access_token'])
-
       retries = 2
       begin
         lolimage = File.new(self.runner.main_image)
-        if client.create_message(post, :attachment1 => lolimage)
+        if yammer_client.create_message(post, :attachment1 => lolimage)
           puts "\t--> Status posted!" unless self.runner.capture_stealth
         end
       rescue => e
