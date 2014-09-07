487a4f19d2d4a16cf7a7cb5e0cd717aa303ba5b0
Matthew Hutchinson <matt@hiddenloop.com>
2014-09-08 00:05:54 +0100

    Fixes #215 (hopefully) sets mplayer fps, and uses last captured frame

--- lib/lolcommits/capturer/capture_linux.rb
+++ lib/lolcommits/capturer/capture_linux.rb
@@ -1,6 +1,8 @@
 # -*- encoding : utf-8 -*-
 module Lolcommits
   class CaptureLinux < Capturer
+    MPLAYER_FPS = 25
+
     def capture_device_string
       @capture_device.nil? ? nil : "-tv device=\"#{@capture_device}\""
     end
@@ -13,13 +15,13 @@ module Lolcommits
       delay = if capture_delay != 0 then capture_delay else 1 end
 
       # There's no way to give a capture delay in mplayer, but a number of frame
-      # mplayer's "delay" is actually a number of frames at 25fps
+      # mplayer's "delay" is actually a number of frames at 25 fps
       # multiply the set value (in seconds) by 25
-      frames = delay.to_i * 25
+      frames = delay.to_i * MPLAYER_FPS
 
       debug 'LinuxCapturer: calling out to mplayer to capture image'
       # mplayer's output is ugly and useless, let's throw it away
-      _, r, _ = Open3.popen3("#{executable_path} -vo jpeg:outdir=#{tmpdir} #{capture_device_string} -frames #{frames} tv://")
+      _, r, _ = Open3.popen3("#{executable_path} -vo jpeg:outdir=#{tmpdir} #{capture_device_string} -frames #{frames} -fps #{MPLAYER_FPS} tv://")
       # looks like we still need to read the output for something to happen
       r.read
 
@@ -27,7 +29,13 @@ module Lolcommits
       include FileUtils::Verbose if logger.level == 0
 
       debug 'LinuxCapturer: calling out to mplayer to capture image'
-      FileUtils.mv(tmpdir + '/%08d.jpg' % frames, snapshot_location)
+
+      # get last frame from tmpdir (regardless of fps)
+      all_frames = Dir.glob("#{tmpdir}/*.jpg").sort_by do |f|
+        File.mtime(f)
+      end
+
+      FileUtils.mv(all_frames.last, snapshot_location)
       debug 'LinuxCapturer: cleaning up'
       FileUtils.rm_rf(tmpdir)
     end
