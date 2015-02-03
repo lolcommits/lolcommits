6d80cacbb93e372a0207d6e907c10dc7ac1d18af
Ben Rexin <ben@rexin.at>
2015-02-03 15:00:35 +0100

    fix adjectives list

--- lib/lolcommits/plugins/lol_protonet.rb
+++ lib/lolcommits/plugins/lol_protonet.rb
@@ -37,7 +37,7 @@ module Lolcommits
     end
 
     def random_adjective
-      adjectives = %w{"awesome", "great", "interesting", "cool", "EPIC", "gut", "good", "pansy",
+      adjectives = ["awesome", "great", "interesting", "cool", "EPIC", "gut", "good", "pansy",
                       "powerful", "boring", "quirky", "untested", "german", "iranian", "neutral", "crazy", "well tested",
                       "jimmy style", "nasty", "bibliographical (we received complaints about the original wording)",
                       "bombdiggidy", "narly", "spiffy", "smashing", "xing style",
@@ -47,7 +47,7 @@ module Lolcommits
                       "- ze goggles zey do nothing! -", "almost working", "legen- wait for it -", "-dairy!",
                       " - Tavonius would be proud of this - ", "Meg FAILMAN!", "- very brofessional of you -",
                       "heartbleeding", "juciy", "supercalifragilisticexpialidocious", "failing", "loving"
-                   }
+                   ]
       adjectives.sample
     end
 
