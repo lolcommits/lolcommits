fb320dad3b0352042695b1f21189cbc384c95fb4
Matthew Rothenberg <mrothenberg@gmail.com>
2015-02-12 12:26:07 -0500

    add YARD documentation with type annotations

--- lib/lolcommits/platform.rb
+++ lib/lolcommits/platform.rb
@@ -3,6 +3,8 @@ require 'mini_magick'
 
 module Lolcommits
   class Platform
+    # A convenience name for the platform.  Mainly used for metaprogramming.
+    # @return String
     def self.platform
       if platform_fakeplatform?   then ENV['LOLCOMMITS_FAKEPLATFORM']
       elsif platform_fakecapture? then 'Fake'
@@ -15,31 +17,45 @@ module Lolcommits
       end
     end
 
+    # Are we on a Mac platform?
+    # @return Boolean
     def self.platform_mac?
       RUBY_PLATFORM.to_s.downcase.include?('darwin')
     end
 
+    # Are we on a Linux platform?
+    # @return Boolean
     def self.platform_linux?
       RUBY_PLATFORM.to_s.downcase.include?('linux')
     end
 
+    # Are we on a Windows platform?
+    # @return Boolean
     def self.platform_windows?
       !!RUBY_PLATFORM.match(/(win|w)32/)
     end
 
+    # Are we on a Cygwin platform?
+    # @return Boolean
     def self.platform_cygwin?
       RUBY_PLATFORM.to_s.downcase.include?('cygwin')
     end
 
+    # Are we currently faking webcam capture for test purposes?
+    # @return Boolean
     def self.platform_fakecapture?
       (ENV['LOLCOMMITS_FAKECAPTURE'] == '1' || false)
     end
 
+    # Are we currently overriding and faking the platform entirely?
+    # @todo possibly make this a private method?
+    # @return Boolean
     def self.platform_fakeplatform?
       ENV['LOLCOMMITS_FAKEPLATFORM']
     end
 
     # Is the platform capable of capturing animated GIFs from webcam?
+    # @return Boolean
     def self.can_animate?
       # FIXME: this is currently matching against string instead of
       # simply returning platform_mac? || platform_linux?
@@ -50,6 +66,7 @@ module Lolcommits
     end
 
     # Is a valid install of imagemagick present on the system?
+    # @return Boolean
     def self.valid_imagemagick_installed?
       return false unless self.command_which('identify')
       return false unless self.command_which('mogrify')
@@ -62,16 +79,21 @@ module Lolcommits
 
     # Is a valid install of ffmpeg present on the system?
     #
-    # For now, this just checks for presence, any version should work.
+    # @note For now, this just checks for presence, any version should work.
+    # @return Boolean
     def self.valid_ffmpeg_installed?
       self.command_which('ffmpeg')
     end
 
     # Cross-platform way of finding an executable in the $PATH.
-    # idea taken from http://bit.ly/qDaTbY, if only_path is true, only the path
+    #
+    # Idea taken from http://bit.ly/qDaTbY, if only_path is true, only the path
     # is returned (not the path and command)
     #
-    #   which('ruby') #=> /usr/bin/ruby
+    # @example
+    #   command_which('ruby') #=> /usr/bin/ruby
+    #
+    # @return Boolean
     def self.command_which(cmd, only_path = false)
       exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
       ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
@@ -92,13 +114,16 @@ module Lolcommits
     #
     # This helper method let's us check for that error condition so we can
     # alert the user in the CLI tool.
+    #
+    # @return Boolean
     def self.git_config_color_always?
       `git config color.ui`.chomp =~ /always/
     end
 
     # Returns a list of system camera devices in a format suitable to display
-    # to the user. Currently only functions on Mac.
+    # to the user.
     #
+    # @note Currently only functions on Mac.
     # @return String
     def self.device_list
       # TODO: handle other platforms here (linux/windows)
