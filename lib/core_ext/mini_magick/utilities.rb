defd18b42623f980e862049b99c8570efc8f229f
Matthew Hutchinson <matt@hiddenloop.com>
2015-04-19 11:58:44 +0100

    fixing windows mini_magick escape issues

---
+++ lib/core_ext/mini_magick/utilities.rb
@@ -0,0 +1,20 @@
+# -*- encoding : utf-8 -*-
+module CoreExt
+  module MiniMagick
+    module Utilities
+      class << self
+        # fixes issue introduced in this commit
+        # https://github.com/minimagick/minimagick/commit/65b6427395cbfe6
+        def windows_escape(cmdline)
+          '"' + cmdline.gsub(/\\(?=\\*\")/, '\\\\\\').gsub(/\"/, '\\"').gsub(/\\$/, '\\\\\\').gsub('%', '%%') + '"'
+        end
+      end
+    end
+  end
+end
+
+# To maintain MiniMagick compatibility on Windows for v3.8.1 we need this patch
+# If/when we upgrade MiniMagick to 4.2+ this patch can be removed
+# We are locked at v3.8.1 since MiniMagick 4+ dropped support for Ruby 1.8.7
+
+MiniMagick.send(:include, CoreExt::MiniMagick::Utilities)
