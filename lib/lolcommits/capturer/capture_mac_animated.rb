3a27cfffbe1698704c31fcaaee2765ab9ddd918e
a06kin <a06kin@gmail.com>
2015-12-18 17:28:38 +0200

    pass fps to ffmpeg output stream (mac)

--- lib/lolcommits/capturer/capture_mac_animated.rb
+++ lib/lolcommits/capturer/capture_mac_animated.rb
@@ -9,14 +9,15 @@ module Lolcommits
       # capture the raw video with videosnap
       system_call "#{executable_path} -s 240 #{capture_device_string}#{capture_delay_string}-t #{animated_duration} --no-audio #{video_location} > /dev/null"
       return unless File.exist?(video_location)
+      # get fps for ffmpeg output stream configuration
+      fps = video_fps(video_location)
       # convert raw video to png frames with ffmpeg
-      system_call "ffmpeg -v quiet -i #{video_location} -t #{animated_duration} #{frames_location}/%09d.png"
+      system_call "ffmpeg -v quiet -i #{video_location} -t #{animated_duration} -r #{fps} #{frames_location}/%09d.png"
 
       # use fps to set delay and number of frames to skip (for lower filesized gifs)
-      fps   = video_fps(video_location)
       skip  = frame_skip(fps)
       delay = frame_delay(fps, skip)
-      debug "Capturer: anaimated gif choosing every #{skip} frames with a frame delay of #{delay}"
+      debug "Capturer: animated gif choosing every #{skip} frames with a frame delay of #{delay}"
 
       # create the looping animated gif from frames (picks nth frame with seq)
       seq_command = "seq -f #{frames_location}/%09g.png 1 #{skip} #{Dir["#{frames_location}/*"].length}"
