c8555f50c399a5a6ddb02d05b61187dc815c9055
Gregory Wehmeier <wehmenstein@gmail.com>
2016-02-11 16:18:21 -0800

    delete extra spacing

--- lib/lolcommits/plugins/uploldz.rb
+++ lib/lolcommits/plugins/uploldz.rb
@@ -40,8 +40,6 @@ module Lolcommits
       log_error(e, "ERROR: RestClient POST FAILED #{e.class} - #{e.message}")
     end
 
-
-
     def configured?
       !configuration['enabled'].nil? && configuration['endpoint']
     end
