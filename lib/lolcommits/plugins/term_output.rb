892598b568c0058ded10322c8c58db3abc62b5d9
Ruxton <ruxton@thefrontiergroup.com.au>
2016-07-26 10:13:03 +0800

    Add output for terminal

---
+++ lib/lolcommits/plugins/term_output.rb
@@ -0,0 +1,37 @@
+# -*- encoding : utf-8 -*-
+require 'rest_client'
+require 'base64'
+
+module Lolcommits
+  class TermOutput < Plugin
+
+    def initialize(runner)
+      super
+    end
+
+    def run_postcapture
+      return unless valid_configuration?
+
+      if !runner.vcs_info || runner.vcs_info.repo.empty?
+        puts 'Repo is empty, skipping output'
+      else
+        base64 = Base64.encode64(open(runner.main_image) { |io| io.read })
+
+        puts "\e]1337;File=inline=1:#{base64};alt=#{runner.meessage}\a\n"
+      end
+
+    end
+
+    def configured?
+      !configuration['enabled'].nil?
+    end
+
+    def self.name
+      'term_output'
+    end
+
+    def self.runner_order
+      :postcapture
+    end
+  end
+end
