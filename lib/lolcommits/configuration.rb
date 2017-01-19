7eeef6effea8eab9b9019b21cd225f67d0f8c474
Matthew Hutchinson <matt@hiddenloop.com>
2017-01-19 14:51:25 +0000

    use YAML.safe_load

--- lib/lolcommits/configuration.rb
+++ lib/lolcommits/configuration.rb
@@ -14,7 +14,7 @@ module Lolcommits
 
     def read_configuration
       return unless File.exist?(configuration_file)
-      YAML.load(File.open(configuration_file))
+      YAML.safe_load(File.open(configuration_file))
     end
 
     def configuration_file
