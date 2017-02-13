14db5114a6085c445668aa76fa43ce311f130e60
Matthew Hutchinson <matt@hiddenloop.com>
2017-02-13 21:27:02 +0000

    rename active method to required

--- lib/lolcommits/gem_plugin.rb
+++ lib/lolcommits/gem_plugin.rb
@@ -1,20 +1,19 @@
 module Lolcommits
   class GemPlugin
-    attr_accessor :name, :gem_name, :spec, :active
+    attr_accessor :name, :gem_name, :spec, :required
 
     def initialize(name, gem_name, spec)
       @name     = name
       @gem_name = gem_name
       @spec     = spec
-      @active   = false
+      @required = false
     end
 
     # activate the plugin (require the gem - enables/loads the plugin
-    # immediately at point of call) Does not reload plugin if it's already
-    # active.
+    # immediately at point of call if not already required)
     def activate!
       begin
-        require gem_path unless active?
+        require gem_path unless required?
       rescue LoadError => e
         warn "Found plugin #{gem_name}, but could not require '#{gem_name}'"
         warn e
@@ -22,12 +21,13 @@ module Lolcommits
         warn "require '#{gem_name}' # Failed, saying: #{e}"
       end
 
-      self.active = true
+      self.required = true
     end
 
-    alias active? active
+    alias required? required
 
     def supported?
+      # false if the plugin gem does not support this version of Lolcommits
       lolcommits_version = Gem::Version.new(::Lolcommits::VERSION)
       spec.dependencies.each do |dependency|
         if dependency.name == Lolcommits::GEM_NAME
